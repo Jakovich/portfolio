edeb8f08bef553198e377b2f8493223db6ee29cc
Nadia Pillard <pillard.nadia@gmail.com>
2017-06-10 17:05:08 +0200

    Update 2017-06-10T15:04:58.112Z

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
   var mainHeader = $('.main-header');
   $(window).scroll(function(){
-    $(this).scrollTop() > $('#intro').innerHeight() - 60 ? mainHeader.addClass('main-header--dark', 'fast') :  mainHeader.removeClass('main-header--dark', 'fast');
-
+    //$(this).scrollTop() > $('#intro').innerHeight() - 60 ? mainHeader.addClass('main-header--dark', 'fast') :  mainHeader.removeClass('main-header--dark', 'fast');
+    checkHeight();
   });
+  checkHeight();
+  function checkHeight() {
+    $(this).scrollTop() > $('#intro').innerHeight() - 60 ? mainHeader.addClass('main-header--dark', 'fast') :  mainHeader.removeClass('main-header--dark', 'fast');
+  }
   $('.navbar-nav a[href^="#"]').click(function () {
      var elementClick = $(this).attr("href");
      var destination = $(elementClick).offset().top - 80;
        $('html').animate( { scrollTop: destination }, 1100 );
        $('body').animate( { scrollTop: destination }, 1100 );
    });
-   
+
 })
